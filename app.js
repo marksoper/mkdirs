020ed6b406e809b4d3a66e02892a4d9c61e07420
Mark Soper <masoper@gmail.com>
2011-08-31 10:37:59 -0400

    adding getUrl

--- app.js
+++ app.js
@@ -1,3 +1,42 @@
 
+var SHORTCUT_REGEX = RegExp('<[^<>]*rel="shortcut icon"[^<>]*>',"i","m");
+var HREF_REGEX = RegExp('href="([^"]*)"',"i","m");
 
-// app here
+
+var getUrl = function(url) {
+    
+    // TO DO:  validate URL
+
+    $.get(url, function(html) {
+        getFavicon(html, function(favicon_url) {
+    	    $("#chooser").html('<img src="' + favicon_url + '" class="favicon"');
+	});
+    })
+    .error(alert("url not found"));
+
+};
+
+var getFavicon = function(html) {
+    var shortcut_link = SHORTCUT_REGEX.exec(html);
+    if (shortcut_link) {
+	var favicon_url = HREF_REGEX.exec(shortcut_link[0]);
+	if (favicon_url) {
+	    return favicon_url[1];
+	}
+    }
+    return null;
+}
+    
+    
+
+
+};
+
+$(function() {
+
+	$("#urlInputButton").live("click", function(evt) {
+		var url = $("#urlInput").val();
+		urlFetcher(url);
+	    });
+
+});
