60eb48a169a7b959aaad7a9ba92465a569055c76
Mark Soper <masoper@gmail.com>
2011-09-01 16:40:04 -0400

    most of app.js moved to backend - e.g. favicon handler

--- app.js
+++ app.js
@@ -1,42 +1,15 @@
 
-var SHORTCUT_REGEX = RegExp('<[^<>]*rel="shortcut icon"[^<>]*>',"i","m");
-var HREF_REGEX = RegExp('href="([^"]*)"',"i","m");
-
-
-var getUrl = function(url) {
     
-    // TO DO:  validate URL
-
-    $.get(url, function(html) {
-        getFavicon(html, function(favicon_url) {
-    	    $("#chooser").html('<img src="' + favicon_url + '" class="favicon"');
-	});
-    })
-    .error(alert("url not found"));
-
-};
-
-var getFavicon = function(html) {
-    var shortcut_link = SHORTCUT_REGEX.exec(html);
-    if (shortcut_link) {
-	var favicon_url = HREF_REGEX.exec(shortcut_link[0]);
-	if (favicon_url) {
-	    return favicon_url[1];
-	}
-    }
-    return null;
-}
-    
-    
-
-
-};
 
 $(function() {
 
-	$("#urlInputButton").live("click", function(evt) {
-		var url = $("#urlInput").val();
-		urlFetcher(url);
-	    });
+    $("#searchButton").live("click", function(evt) {
+        var input = $("#searchInput").val();
+	$.get("http://localhost:8080/q?"+encodeURIComponent(input), function(data) {
+	    var results = JSON.parse(data);
+	    alert(results['favicon_url']);
+	    $("#chooser").html('<img src="' + results['favicon_url'] + '" class="favicon"');
+	});
+    });
 
 });
